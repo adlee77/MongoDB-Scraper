85286b3216be0efefbbf31fef6596c5d13b6cfd4
Andrew Lee <andrewimg@gmail.com>
2020-06-18 22:36:34 -0700

    added viewing of notes and working on edit and delete button for notes

--- public/app.js
+++ public/app.js
@@ -48,6 +48,7 @@ $(".saveNote").on("click", function() {
     })
 })
 $(".view").on("click", function(){
+    $('.modal-body').empty()
     const dataId = $(this).attr("data-id")
     const newsTitle = $(this).attr("news-title")
     $(".modal-title-id").html(dataId)
@@ -56,6 +57,30 @@ $(".view").on("click", function(){
         method: "GET",
         url: "/note/saved/" + $(".modal-title-id").html(),
     }).then((data)=>{
-        console.log(data[0].subject)
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].subject)
+            console.log(data[i].body)
+            console.log(data[i].id)
+            $('.modal-body-saved').append(`<div class="card noteCard">
+            <div class="row">
+                <div class="col-6">
+                    <h4 class="noteSubject">${data[i].subject}:</h4>
+                </div>
+                <div class="col-2">
+                    <p id="note-id">${data[i].id}</p>
+                </div>
+                <div class="col-2">
+                    <button id="edit" class="btn btn-secondary">Edit</button>
+                </div>
+                <div class="col-2">
+                    <button id="deleteNote" class="btn btn-secondary">Delete</button>
+                </div>
+            </div>
+                <p>${data[i].body}</p></div>`)
+        }
     })
+})
+
+$("#deleteNote").on("click", ()=>{
+
 })
